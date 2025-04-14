36867cce2970f0fc71429aa45b5acbd13d1ba25b
Shanmukh-235 <shanmukhapoornachand14316@gmail.com>
2025-04-14 20:34:30 +0530

    Application completed, partial changes were in pending

--- app.js
+++ app.js
@@ -5,5 +5,15 @@ function generateQR(){
     if(qrText.value.length > 0){
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
     imgBox.classList.add("show-img");
+    } else {
+       qrText.classList.add('error');
+       setTimeout(()=> {
+         qrText.classList.remove('error')
+       },1000);
     }
-}
+} 
+qrText.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        generateQR();
+    }
+});
